700017c7c4a119effc8ff848c03c3cb9300a1c24
larsjaeger <larsjgr@gmail.com>
2020-11-07 00:29:11 +0100

    added multithreading

--- primeNumbers.c
+++ primeNumbers.c
@@ -2,7 +2,23 @@
 #include<stdlib.h>
 #include<time.h>
 #include<math.h>
+#include <pthread.h>
 
+
+// config
+static const double upperEnd = 150.0;
+static const double lowerEnd = 99.0;
+static double currentNumber = lowerEnd;
+
+
+static double getNextNumber() {
+    if(currentNumber > upperEnd) {
+        return 0;
+    }
+    double nr = currentNumber;
+    currentNumber++;
+    return nr;
+}
 
 int primetester(double pn) {
     double rechenNummer;
@@ -21,21 +37,37 @@ int primetester(double pn) {
     }
     return 1;
 }
+void *testRemainingNumbers() {
+    double currentThreadNumber = getNextNumber();
+    while(currentThreadNumber != 0) {
+        if (primetester(currentThreadNumber) == 1) {
+            printf("%.0f \n", currentThreadNumber);
+        }
+        currentThreadNumber = getNextNumber();
+    }
+    return NULL;
+}
 
 int main() {
+    //timing start
     clock_t tv1, tv2;
     double time;
     tv1 = clock();
 
-    double upperEnd = 1500000000000000.0;
-    double lowerEnd = 999999999990000.0;
-    for(double i = lowerEnd; i <= upperEnd; i++) {
-        if (primetester(i) == 1) {
-            printf("%f \n", i);
-        }
+    //multithreading setup
+    pthread_t threads[4];
+
+    //thread starter
+    for(int i = 0; i < sizeof(threads) / sizeof(threads[0]); i++) {
+        printf("%i \n", i);
+        pthread_create(&threads[i], NULL, &testRemainingNumbers, NULL);
+    }
+    //thread stopper
+    for(int i = 0; i < sizeof(threads) / sizeof(threads[0]); i++) {
+        pthread_join(threads[i], NULL);
     }
 
-
+    //end of timing
     tv2 = clock();
     time = (tv2 - tv1) / (CLOCKS_PER_SEC / (double) 1000.0);
     printf("time = %f\n", time);
